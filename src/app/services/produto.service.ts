ef211c38eba6695d17c1b1f4ec967b364231fefa
Yara Gomes <101425183+gomesyara@users.noreply.github.com>
2023-04-28 23:04:20 +0000

    aula 28/04

--- src/app/services/produto.service.ts
+++ src/app/services/produto.service.ts
@@ -1,17 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-
-@Injectable({
-  providedIn: 'root'
-})
-
 interface Produto{
   idproduto: number,
   fkmarca: number,
   nome: string,
   preco: number
 }
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
 export class ProdutoService {
 
   constructor(private http: HttpClient) { }
